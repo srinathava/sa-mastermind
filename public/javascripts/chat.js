32e3f6c08f15afd9c8fcc70d843a4c9ae44ba7cb
Srinath Avadhanula <srinathava@gmail.com>
2020-03-25 21:13:51 -0400

    missing files

---
+++ public/javascripts/chat.js
@@ -0,0 +1,36 @@
+class Chat {
+
+    append(name, text) {
+        let newDiv = $('<div class="message">');
+        let nameDiv = $('<div class="name">').text(name);
+        let textDiv = $('<div class="dialog">').text(text);
+
+        newDiv.append(nameDiv);
+        newDiv.append(textDiv);
+
+        $('#chat').append(newDiv);
+    }
+
+    constructor(io, name) {
+
+        $("#input").keypress(function (e) {
+            var code = (e.keyCode ? e.keyCode : e.which);
+            if (code == 13) {
+                let text = e.currentTarget.value;
+                e.currentTarget.value = "";
+                e.preventDefault();
+
+                io.emit('chat', {name, text});
+                return true;
+            }
+        });
+
+        io.on('chat', data => {
+            let {name, text} = data;
+            this.append(name, text);
+        });
+
+    }
+}
+
+export { Chat };
